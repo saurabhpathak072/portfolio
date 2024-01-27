9236ce6bd9ff5fbab8cc8179b6f18467c00472e3
Saurabh Pathak <SPathak@eagle.org>
2024-01-28 01:51:04 +0530

    fixed the es Lint error

--- src/Components/App/Project/ProjectCard/ProjectCard.jsx
+++ src/Components/App/Project/ProjectCard/ProjectCard.jsx
@@ -1,12 +1,12 @@
-import React, { useContext } from "react";
+import React from "react";
 import {  Col, Container, Row } from "react-bootstrap";
 // import { projectList } from "../../../../Data/data";
 import SkillCards from "../../../UI/Cards/SkillCards";
 import styles from "./ProjectCard.module.css";
-import { AppContext } from "../../../../Context/Context";
+
 
 const ProjectCard = ({projectList}) => {
-  const {data} = useContext(AppContext)
+
   const project = projectList.map((prj, index) => {
     return (
       <Col className={`py-2 my-2 ${styles.ProjectCard}`} key={index} lg={6}>
